64f6d0a65d3a313ebc2754540a93b4800c4dd09f
tiancai <276448724@qq.com>
2022-02-10 16:58:28 +0800

    8.主模块及测试运行

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ class App(wx.App):
     def onInit(self):
         frame=ui_login.LoginWindow(parent=None,title='系统登录');
         frame.Show();
-        frame.Centre();
+        frame.Centre()
         return True
 if __name__ == '__main__':
    app=App()
